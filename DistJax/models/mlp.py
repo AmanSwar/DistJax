8de9cf31c8f77eca3ce36b76b0ee4c1152894824
Aman <aman.swar001@gmail.com>
2025-08-25 13:45:36 +0530

    mv TPMLPBLock and TPMLPLayer (tp_classifier -> mlp)

--- DistJax/models/mlp.py
+++ DistJax/models/mlp.py
@@ -89,3 +89,57 @@ class MLPBlockOutput(nn.Module):
             name="dense",
         )(x)
         return x
+
+
+class TPMLPBlock(nn.Module):
+    config: ConfigDict
+    train: bool
+
+    @nn.compact
+    def __call__(self, x: jax.Array) -> jax.Array:
+        tp_size = jax.lax.psum(1, self.config.model_axis_name)
+        input_features = x.shape[-1]
+        # Input layer
+        x = TPDense(
+            dense_fn=functools.partial(
+                MLPBlockInput,
+                config=self.config,
+                features=self.config.hidden_size * self.config.mlp_expansion // tp_size,
+            ),
+            model_axis_name=self.config.model_axis_name,
+            tp_mode="gather",
+            name="input",
+        )(x)
+        # Output layer
+        x = TPDense(
+            dense_fn=functools.partial(
+                MLPBlockOutput,
+                config=self.config,
+                features=input_features * tp_size,
+            ),
+            model_axis_name=self.config.model_axis_name,
+            tp_mode="scatter",
+            kernel_init_adjustment=tp_size**-0.5,  # fan-in with tp_size fewer inputs.
+            name="output",
+        )(x)
+        return x
+
+
+class TPMLPLayers(nn.Module):
+    config: ConfigDict
+    train: bool
+    block_class: Callable[..., nn.Module] = TPMLPBlock
+
+    @nn.compact
+    def __call__(self, x: jax.Array) -> jax.Array:
+        module = self.block_class(config=self.config, train=self.train, name="block")
+        x, _ = nn.scan(
+            lambda module, carry, _: (module(carry) + carry, None),
+            variable_axes={"params": 0},
+            split_rngs={"params": True, "dropout": True},
+            length=self.config.num_layers,
+            metadata_params={
+                "partition_name": None
+            },  # We do not need to partition the parameters over the layer axis.
+        )(module, x, ())
+        return x
