a6d46c14a3c912a67efbec83d371f53761c18451
Aman <aman.swar001@gmail.com>
2025-08-25 08:28:14 +0530

    add function train_step_dp

--- DistJax/parallelism/data_parallel.py
+++ DistJax/parallelism/data_parallel.py
@@ -1,10 +1,11 @@
 import jax
+import jax.numpy as jnp
 import flax.linen as nn
 
-from typing import Sequence
+from typing import Sequence , Tuple
+from ml_collections import ConfigDict
 
-from DistJax.core.training import PyTree , Parameter
-
+from DistJax.core.training import PyTree , Parameter , TrainState , Metrics , Batch , accum_grads
 
 def sync_grads(grads: PyTree, axis_names=Sequence[str]):
     def _sync_grads(g: Parameter):
@@ -29,3 +30,35 @@ def sync_grads(grads: PyTree, axis_names=Sequence[str]):
     return jax.tree_util.tree_map(
         _sync_grads, grads, is_leaf=lambda x: isinstance(x, nn.Partitioned)
     )
+
+
+def train_step_dp(
+    state: TrainState, metrics: Metrics, batch: Batch , loss_fn , CONFIG : ConfigDict
+) -> Tuple[TrainState, Metrics]:
+
+    rng, step_rng = jax.random.split(state.rng)
+
+    grads, step_metrics = accum_grads(
+        state, batch, step_rng, CONFIG.optimizer.num_minibatches, loss_fn=loss_fn
+    )
+
+    with jax.named_scope("sync_grads"):
+        grads = jax.tree_util.tree_map(
+            lambda g: jax.lax.pmean(g, axis_name=CONFIG.data_axis_name), grads
+        )
+
+    new_state = state.apply_gradients(grads=grads, rng=rng)
+
+    with jax.named_scope("sync_metrics"):
+
+        step_metrics = jax.tree_util.tree_map(
+            lambda x: jax.lax.psum(x, axis_name=CONFIG.data_axis_name), step_metrics
+        )
+
+    if metrics is None:
+        metics = step_metrics
+
+    else:
+        metrics = jax.tree_util.tree_map(jnp.add, metrics, step_metrics)
+
+    return new_state, metrics
