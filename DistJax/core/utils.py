83e1579aa1416f720ad103e1b1ceba84d2afadab
Aman <aman.swar001@gmail.com>
2025-08-25 14:04:11 +0530

    add split_array_over_mesh function

--- DistJax/core/utils.py
+++ DistJax/core/utils.py
@@ -14,3 +14,16 @@ def scale_init(init_fn: Callable, scale_factor: float = 1.0):
         return scale_factor * init_fn(rng, *args, **kwargs)
 
     return _init_fn
+
+
+def split_array_over_mesh(x: jax.Array, axis_name: str, split_axis: int) -> jax.Array:
+    axis_size = jax.lax.psum(1, axis_name)
+    axis_index = jax.lax.axis_index(axis_name)
+    slice_size = x.shape[split_axis] // axis_size
+    x = jax.lax.dynamic_slice_in_dim(
+        x,
+        axis_index * slice_size,
+        slice_size,
+        axis=split_axis,
+    )
+    return x
