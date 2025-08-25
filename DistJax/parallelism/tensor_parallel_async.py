7f3242c578350d75c3c72742376b01613a1e69db
Aman <aman.swar001@gmail.com>
2025-08-25 13:20:50 +0530

    add function async_gather and async_gather_bidirectional

---
+++ DistJax/parallelism/tensor_parallel_async.py
@@ -0,0 +1,54 @@
+from functools import partial
+from typing import Any, Callable, Dict, List, Literal, Sequence, Tuple
+
+import flax.linen as nn
+import jax
+import jax.numpy as jnp
+from ml_collections import ConfigDict
+
+from DistJax.parallelism.pipeline_parallel import ModelParallelWrapper
+from DistJax.models.mlp import MLPBlockInput , MLPBlockOutput
+from DistJax.core.utils import scale_init
+from DistJax.core.training import PyTree , Parameter , Metrics
+
+
+def async_gather(x: PyTree, axis_name: str, shift_up: bool = True) -> List[PyTree]:
+
+    tp_size = jax.lax.psum(1, axis_name)
+    if shift_up:
+        shift_perm = [(j, (j + 1) % tp_size) for j in range(tp_size)]
+    else:
+        shift_perm = [(j, (j - 1) % tp_size) for j in range(tp_size)]
+    ps = [x]
+    p = x
+    for _ in range(1, tp_size):
+        p = jax.lax.ppermute(p, axis_name, perm=shift_perm)
+        ps.append(p)
+    return ps
+
+
+def async_gather_bidirectional(
+    x: jax.Array, axis_name: str, shift_up: bool = True
+) -> List[jax.Array]:
+
+    tp_size = jax.lax.psum(1, axis_name)
+    shift_up_perm = [(j, (j + 1) % tp_size) for j in range(tp_size)]
+    shift_down_perm = [(j, (j - 1) % tp_size) for j in range(tp_size)]
+    ps_up = []
+    ps_down = []
+    p_up = x
+    p_down = x
+
+    for i in range(1, tp_size):
+        if i % 2 == 0:
+            p_down = jax.lax.ppermute(p_down, axis_name=axis_name, perm=shift_down_perm)
+            ps_down.append(p_down)
+        else:
+            p_up = jax.lax.ppermute(p_up, axis_name=axis_name, perm=shift_up_perm)
+            ps_up.append(p_up)
+
+    if shift_up:
+        ps = [x] + ps_up + ps_down[::-1]
+    else:
+        ps = [x] + ps_down + ps_up[::-1]
+    return ps
