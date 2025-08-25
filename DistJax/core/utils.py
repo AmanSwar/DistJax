fba48636416b4ccbbd607400dca67e90e378c07a
Aman <aman.swar001@gmail.com>
2025-08-25 08:35:43 +0530

    add function scale_init

--- DistJax/core/utils.py
+++ DistJax/core/utils.py
@@ -1,7 +1,16 @@
 import jax
 
+from typing import Callable
 
 def fold_rng_over_axis(rng: jax.random.PRNGKey, axis_name: str):
 
     axis_index = jax.lax.axis_index(axis_name)
     return jax.random.fold_in(rng, axis_index)
+
+
+def scale_init(init_fn: Callable, scale_factor: float = 1.0):
+
+    def _init_fn(rng, *args, **kwargs):
+        return scale_factor * init_fn(rng, *args, **kwargs)
+
+    return _init_fn
