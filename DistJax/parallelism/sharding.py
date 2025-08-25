35ef3f29c43a0cfc5f671b899356bf57150e412b
Aman <aman.swar001@gmail.com>
2025-08-25 08:16:05 +0530

    add function shard_params

---
+++ DistJax/parallelism/sharding.py
@@ -0,0 +1,59 @@
+import jax
+import flax.linen as nn
+from jax import lax
+
+import logging
+
+from DistJax.core.training import PyTree , Parameter
+
+
+
+@jax.named_scope("shard_params")
+def shard_params(
+    params: PyTree, axis_name: str, min_weight_size: int = 2**18
+) -> PyTree:
+
+    axis_idx = jax.lax.axis_index(axis_name)
+    axis_size = jax.lax.psum(1, axis_name)
+
+    def _split(x: Parameter) -> Parameter:
+        if isinstance(x, nn.Partitioned):
+            value, names = x.value, x.names
+        else:
+            value = x
+            names = (None,) * value.ndim
+        if axis_name in names:
+            logging.warning(
+                f"Parameter {value.shape} with names {names} already sharded on axis {axis_name}."
+            )
+            return x
+        elif value.size <= min_weight_size:
+            logging.info(
+                f"Parameter {value.shape} with names {names} too small to shard, size {value.size} < {min_weight_size}."
+            )
+            return x
+        else:
+            shape = value.shape
+            idx = np.argsort(shape)[::-1]  # Shard along largest possible axis.
+            for i in idx:
+                if shape[i] % axis_size == 0 and names[i] is None:
+                    split_size = shape[i] // axis_size
+                    p_sharded = nn.Partitioned(
+                        value=lax.dynamic_slice_in_dim(  # Shard to keep on present device.
+                            value, axis_idx * split_size, split_size, axis=i
+                        ),
+                        names=names[:i] + (axis_name,) + names[i + 1 :],
+                    )
+                    return p_sharded
+            logging.warning(
+                f"Could not shard {value.shape} with names {names} on axis {axis_name}, no suitable axis found."
+            )
+            return x
+
+    return jax.tree_util.tree_map(
+        _split,
+        params,
+        is_leaf=lambda x: isinstance(
+            x, nn.Partitioned
+        ),  # Consider a nn.Partitioned object as a leaf.
+    )
