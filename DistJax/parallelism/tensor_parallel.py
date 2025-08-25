24a480e99a26e0722a8c3e1f0db089b0cb25a5fe
Aman <aman.swar001@gmail.com>
2025-08-25 12:57:48 +0530

    add function sync grad

--- DistJax/parallelism/tensor_parallel.py
+++ DistJax/parallelism/tensor_parallel.py
@@ -2,11 +2,12 @@ import jax
 import jax.numpy as jnp
 import flax.linen as nn
 
-from typing import Callable , Any , Literal
+from typing import Callable , Any , Literal , Sequence
 from functools import partial
 
 from DistJax.parallelism.pipeline_parallel import ModelParallelWrapper
 from DistJax.core.utils import scale_init
+from DistJax.core.training import PyTree , Parameter
 
 class TPDense(nn.Module):
 
@@ -57,3 +58,28 @@ class TPDense(nn.Module):
             raise ValueError(f"Unknown Tensor Parallel model : {tp_mode}")
 
         return x
+
+
+def sync_grads(grads: PyTree, axis_names=Sequence[str]):
+    def _sync_grads(g: Parameter):
+
+        if isinstance(g, nn.Partitioned):
+            replication_axis_name = [
+                name
+                for name in axis_names
+                if name not in jax.tree_util.tree_leaves(g.names)
+            ]
+
+            if len(replication_axis_name) == 0:
+                return g
+
+            else:
+                return g.replace(
+                    value=jax.lax.pmean(g.value, axis_name=replication_axis_name)
+                )
+        else:
+            return jax.lax.pmean(g, axis_name=axis_names)
+
+    return jax.tree_util.tree_map(
+        _sync_grads, grads, is_leaf=lambda x: isinstance(x, nn.Partitioned)
+    )
